3a4c616b52fb136e70bf12f4573f92870d94e5fa
iAlkhaz <43182778+iAlkhaz@users.noreply.github.com>
2022-04-01 09:35:31 -0300

    Actualización db
    Se indica donde se debe agregar db para futuro tesista

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -3,13 +3,7 @@ import "firebase/firestore";
 import "firebase/auth"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD2jJXbFRv1s6DfNiDuSmZlBytNFit_X8Q",
-    authDomain: "tesis-app-aa0bd.firebaseapp.com", 
-    databaseURL: "https://tesis-app-aa0bd-default-rtdb.firebaseio.com",
-    projectId: "tesis-app-aa0bd",
-    storageBucket: "tesis-app-aa0bd.appspot.com", 
-    messagingSenderId: "493594094497",
-    appId: "1:493594094497:web:13223d223e02a794f9f9e0" 
+   /* Aquí se debe colocar la configuración de la db de firebase*/
     };    
 
 firebase.initializeApp(firebaseConfig);
